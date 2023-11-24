a84a4f316b2d8ae470e40b16638202011aedf15a
Azat222 <azat222@gmail.com>
2023-11-24 20:48:23 +0300

    fix: added putting new poll vallues into created_polls

--- schedules/poll_creator.py
+++ schedules/poll_creator.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 from loader import bot
 from database.SettingsDatabaseManager import SettingsDatabaseManager
+from database.UsersDatabaseManager import UsersDatabaseManager
 
 
 async def create_poll():
-    with SettingsDatabaseManager() as db_worker:
-        for data in db_worker.get_mailing_chats():
-            await bot.send_poll(data[0], question='настроение сегодня',
-                                      options=['+++😇', '++-🥹', '+--🥲', '---😵‍💫'],
-                                      is_anonymous=False)
+    with SettingsDatabaseManager() as settings_db_worker:
+        with UsersDatabaseManager() as users_db_worker:
+            for data in settings_db_worker.get_mailing_chats():
+                created_poll = await bot.send_poll(data[0], question='настроение сегодня',
+                                          options=['+++😇', '++-🥹', '+--🥲', '---😵‍💫'],
+                                          is_anonymous=False)
+                users_db_worker.add_poll(created_poll.chat.id, created_poll.poll.id, datetime.now().strftime("%d.%m"))
